2f8a20bddc488109947b730ace110c91090fa50c
Marie <marie.mimosa.33@gmail.com>
2014-10-17 15:16:42 +0200

    Changement PHP
    Ajout de .gitignore pour ignorer le fichier db.php (à créer)

--- SiteKEEPIN/fonctionphp.php
+++ SiteKEEPIN/fonctionphp.php
@@ -1,21 +1,4 @@
-<?php
-   $serveur='127.0.0.1'; //A changer à chaque fois pour chaque personne
-   $username='root';
-
-   $password='toor';
-   $databasename='K
-   eepin';
-
-   $password='';
-   $databasename='Keepin';
-
-   
-   $idconn=mysqli_connect($serveur,$username,$password);
-   if (mysqli_errno($idconn)){
-   		print(mysqli_error($idconn));}
-   $select_database=mysqli_select_db($idconn,$databasename);
-
-   
+<?php   
    function User_name(){
    		global $idconn;
 		$SQLQuery='select empl_nom,empl_prenom from employe';
