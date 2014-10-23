8cc8b7a1dd772d86f1882739c95c248837f38619
Marie <marie.mimosa.33@gmail.com>
2014-10-23 11:18:00 +0200

    Modif MonCompte

--- SiteKEEPIN/AccueilMonCompte.php
+++ SiteKEEPIN/AccueilMonCompte.php
@@ -12,22 +12,6 @@
 		<section class="paragraphe">
 	
 			<h1 style="margin-left: 145px">Mon compte</h1>
-			<table style="margin-left:40px">
-				<tr>
-					<th>Nom</th>
-					<th>Prénom</th>
-					<th>Service</th>
-					<th>Poste</th>
-					<th>Code d'accés</th>
-				</tr>
-				<tr>
-					<td>Martin</td>
-					<td>Jacques</td>
-					<td>Informatique</td>
-					<td>Developpeur web</td>
-					<td>0718</td>
-				</tr>
-			</table>
 			<?php MonCompte() ?>
 			<a href = "HistoriqueUser.php"><input type="button" id = "button" name="consultermonhistorique" value="Consulter mon historique">
 			<a href = "Accreditation.php"><input type="button" id = "button" name="demandeaccreditations" value="Demande d'accréditations">
