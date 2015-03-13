5b11e86d28cce2556667d9f64cac62d6b4927aba
Alexandre <mybrainisout@gmail.com>
2015-03-13 17:31:10 +0100

    suppr/modif compte
    Champ input à la place d'un select + ajout bouton retour page
    supprimercompte

--- SiteKEEPIN/SupprimerCompte.php
+++ SiteKEEPIN/SupprimerCompte.php
@@ -12,10 +12,11 @@
 		<section class=paragraphe>
 		
 			<h1>Supprimer un compte</h1>
-			<label for="Suppr">Sélectionner le compte :</label>
-			<select name="Suppression" id="Suppr"><br/>
+			<label for="Suppr">Sélectionner le compte :</label><br/>
+			<input type="text" name="Seek" id="seek"/>
+			
 			<a href = "AccueilAdmin.php"><input type="submit" id="submit" value="Valider"; onclick="alert('Attention! Vous êtes sur le point de supprimer ce compte. Êtes-vous sûr?')"/></a>
-		
+		<a href = "AccueilAdmin.php"><input type="button" id="button" value="Retour"/></a>
 		</section>
 	
 	</body>
--- SiteKEEPIN/script/fonctionphp.php
+++ SiteKEEPIN/script/fonctionphp.php
@@ -8,22 +8,7 @@
    }
 	
 	
-	/*function HistoriqueAdmin(){
-		global $idconn;
-		$SQLQuery = 'SELECT empl_nom, empl_prenom, serv_libelle, niv_libelle, post_libelle, port_libelle, dev_dateJour, dev_heure FROM poste INNER JOIN service ON post_serv_ID = serv_ID INNER JOIN employe ON post_ID = empl_post_IDINNER JOIN comporter ON comp_serv_ID = serv_ID INNER JOIN niveau ON comp_niv_ID = niv_IDINNER JOIN deverrouiller ON dev_niv_ID = niv_ID INNER JOIN porte ON dev_port_ID = port_ID';
-		$SQLResult = mysqli_query($idconn, $SQLQuery);
-		$script = '<table>'.'<tr><th>'.'Nom'.'</th>'.'<th>'.'Prénom'.'</th>'.'<th>'.'Service'.'</th>'.'<th>'.'Poste'.'</th>'.'<th>'.'Salle'.'</th>'.'<th>'.'Date'.'</th>'.'<th>'.'Heure'.'</th></tr>';
-		
-		while ($SQLRow = mysqli_fetch_array ($SQLResult)){
-			$script .= '<tr><td>'.utf8_encode($SQLRow['empl_nom']).'</td>'.'<td>'.utf8_encode($SQLRow['empl_prenom']).'</td>'.'<td>'.$SQLRow['serv_libelle'].'</td>';
-			$script .= '<td>'.$SQLRow['niv_libelle'].'</td>'.'<td>'.$SQLRow['post_libelle'].'</td>'.'<td>'.$SQLRow['port_libelle'].'</td>';
-			$script .= '<td>'.$SQLRow['dev_dateJour'].'</td>'.'<td>'.$SQLRow['dev_heure'].'</td></tr>';
-		}
-		$script .= '</table>';
-		print($script);
-		mysqli_free_result($SQLResult);
-		}
-	*/
+
 	
 	
 	
@@ -44,21 +29,7 @@
 		}
 		
 		
-		function AjoutCompte(){
-		$nom = htmlentities($_POST['Nom']);
-		$prenom = htmlentities($_POST['Prenom']);
-		$ddn = htmlentities($_POST['Date_de_Naissance']);
-		$matricule = htmlentities($_POST['Matricule']);
-		$poste = htmlentities($_POST['Poste']);
-		$tel = htmlentities($_POST['Tel']);
-		$mail = htmlentities($_POST['mail']);
-		$mdp = htmlentities($_POST['mdp']);
-		$codepin = htmlentities($_POST['codepin']);
-		$role = htmlentities($_POST['Role']);
-		$acc = htmlentities($_POST['Accreditation']);
 		
-		$SQLQuery = 'INSERT INTO employe (empl_nom, empl_prenom, empl_DDN, empl_matricule, empl_poste_ID, empl_tel, empl_mail, empl_password, empl_codePIN, empl_ro_ID) value ($nom, $prenom, $ddn, $matricule, $poste, $tel, $mail, $mdp, $codepin, $role)'
-	}
 	
 	
    	function dateheure() {
