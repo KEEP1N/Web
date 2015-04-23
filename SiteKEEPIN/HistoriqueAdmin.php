37c0b0f3c8d20831ec0d2dbd27b19ec1dad0563f
Marie <marie.mimosa.33@gmail.com>
2015-04-23 10:40:11 +0200

    Historique

--- SiteKEEPIN/HistoriqueAdmin.php
+++ SiteKEEPIN/HistoriqueAdmin.php
@@ -12,63 +12,53 @@
 		<?php
 			if (isset($_SESSION['empl_mail']) && $_SESSION['empl_ro_ID']==2) :
 		?>
-		<input type="text" list="Batiment "/>
-		<datalist id="Batiment" >
-				<option> Bat B
-				</option>
-				</datalist>
+		
+		<section class="paragraphe">
+	
+			<h1 style="margin-left: 150px">Historique de passage</h1>
+			
+			
+			
+			<?php
+			
+			GLOBAL $idconn;
+			$SQLQueryHisto = "SELECT empl_nom, empl_prenom, dev_heure, dev_dateJour, port_libelle FROM employe INNER JOIN posseder ON empl_ID = poss_empl_id INNER JOIN acces ON poss_acc_id = acc_ID INNER JOIN deverrouiller ON acc_ID = dev_acc_ID INNER JOIN porte ON dev_port_ID = port_ID";
+			$SQLResult = mysqli_query($idconn, $SQLQueryHisto);
+			$script = '<table style="margin-right: 170px">'.'<tr><th>'.'Nom'.'</th>'.'<th>'.'Prénom'.'</th>'.'<th>'.'Porte'.'</th>'.'<th>'.'Date'.'</th>'.'<th>'."Heure".'</th></tr>';
+			
+			$list1[] = array("Nom", "Prénom", "Porte", "Date", "Heure"); 
+			while ($SQLRow = mysqli_fetch_array ($SQLResult)){
+			$script .= '<tr><td>'.utf8_encode($SQLRow['empl_nom']).'</td>'.'<td>'.utf8_encode($SQLRow['empl_prenom']).'</td>';
+			$script .= '<td>'.utf8_encode($SQLRow['port_libelle']).'</td>'.'<td>'.utf8_encode($SQLRow['dev_dateJour']).'</td>';
+			$script .= '<td>'.$SQLRow['dev_heure'].'</td></tr>';
+			$list1[] = array(utf8_encode($SQLRow['empl_nom']),utf8_encode($SQLRow['empl_prenom']),utf8_encode($SQLRow['port_libelle']),utf8_encode($SQLRow['dev_dateJour']),$SQLRow['dev_heure']);
+			
+			}
+			
+			$script .= '</table>';
+			print($script);
 			
 		
-		<input type="text" list="Date" >
-		<datalist id="Date" >
-				<option> 03/2015
-				</option>
-		</datalist>
-		<br/>
-		<a ><input type="button" id="button" value="Historique"/></a>
-		<section class=paragraphe>
-	
-			<h1 style="margin-left: 150px">Historique</h1>
-			<table style="margin-right: 170px">
-				<tr>
-					<th>Porte</th>
-					<th>Nom</th>
-					<th>Prenom</th>
-					<th>Date</th>
-					<th>Heure</th>
-					
-				</tr>
-				<tr>
-					<td>B16</td>
-					<td>Bricou</td>
-					<td>John</td>
-					<td>13/03/2015</td>
-					<td>15h20</td>
-					
-				</tr>
-				<tr>
-					<td>B22</td>
-					<td>Zen</td>
-					<td>Viktor</td>
-					<td>11/03/2015</td>
-					<td>15h55</td>
-					
-				</tr>
-				<tr>
-					<td>B25</td>
-					<td>Bricou</td>
-					<td>John</td>
-					<td>10/03/2015</td>
-					<td>17h05</td>
-					
-				</tr>
-			</table>
+			// Fonction pour exporter en CSV, pas encore correcte donc à modifier quand on aura le temps
+			/*function ouvrir(){
+			GLOBAL $list1;
+			$fp = fopen("export1.csv", "w");
+			foreach($list1 as $fields):
+				fputcsv($fp, $fields);
+			endforeach;
+			fclose($fp);
+			}*/
 			
-			<?php endif;?>
+			mysqli_free_result($SQLResult);
+			?>
+			
+			<a href = "AccueilAdmin.php"><input type="button" id="retour" value=""/></a>
+			<!--<input type="button" value="Exporter en CSV" onclick = <?php ouvrir() ?>/>-->
 
 		</section>
 		
-	<a ><input type="button" id="button" value="Export PDF"/></a>
+		<?php endif;?>
+	
 	</body>
 	
 </html>
