6563bd97d150c8bad517430cf4058f1cd333068d
Marie <marie.mimosa.33@gmail.com>
2015-06-15 15:53:48 +0200

    Indentation + recodage modifMDP
    Indentation du code + recodage de la page modif MDP

--- Code/ModifierMDP.php
+++ Code/ModifierMDP.php
@@ -3,54 +3,29 @@
 	include'script/fonctionphp.php';
 	include'script/db.php';
 	include 'basehtml.php';
-        include 'script/fonction_verification2.php';
+        include 'script/fonction_verification.php';
+        include 'script/fonction_mdp.php';
         
-                if(empty($_SESSION['empl_ID'])) {
-                    header('Location:index.php');
+        if(empty($_SESSION['empl_ID'])) {
+           header('Location:index.php');
         }
 ?>
-
-
-    
 		<section class="paragraphe">
-	
-			<h1> Changement de mot de passe</h1>
-<<<<<<< HEAD
-                        <form method="post" action="ModifierMDP.php" onsubmit ="return verifForm(this)">
-			<label for="lastmdp">Ancien mot de passe: </label><br/><input type="password" onblur="verifChamp(this)" name="lastmdp" /><br/>
-			<label for="newmdp">Nouveau mot de passe: </label><br/><input type="password" onblur="verifChamp(this)" name="newmdp" /><br/>
-			<label for="newmdp">Confirmation du nouveau mot de passe:</label><br/><input type="password" onblur="verifChamp(this)" name="newmdp2" /><br/>
-=======
-                        <form method="post" action="#">
-			<label for="lastmdp">Ancien mot de passe: </label><input type="text" name="lastmdp" /><br/>
-			<label for="newmdp">Nouveau mot de passe: </label><br/><input type="text" name="newmdp" /><br/>
-			<label for="newmdp">Confirmation du nouveau mot de passe:</label><br/><input type="text" name="newmdp2" /><br/>
->>>>>>> 931135be5cdfacf1bd17f696569c146e73263ce7
-			
-                        <a href = "AccueilMonCompte.php"><input type="button" id="retour" value=""/></a>
-			<input type="submit" id="submit" name="validation" value="Modifier"/>
+                    <h1> Changement de mot de passe</h1>
+                        <form method="post" action="ModifierMDP.php" name="formulaire" onsubmit ="return verifMDP(this)">
+                            <label for="lastmdp">Ancien mot de passe: </label>
+                            <input type="password" name="lastmdp" id="lastmdp" onblur="verifChamp(this)" /><br/>
+                            <label for="newmdp">Nouveau mot de passe: </label><br/>
+                            <input type="password" name="newmdp" id="newmdp" onblur="verifChamp(this)"/><br/>
+                            <label for="newmdp">Confirmation du nouveau mot de passe:</label><br/>
+                            <input type="password" name="newmdp2" id="newmdp2" onblur="verifChamp(this)" /><br/>
+                            
+                            <div id="message"><?php modifMDP(); ?></div>
+                            
+                            <a href = "AccueilMonCompte.php"><input type="button" id="retour" value=""/></a>
+                            <input type="submit" id="submit" name="validation" value="Modifier"/>
+                            
                         </form>
-                        <?php 
-                    
-                            if (isset($_POST['validation'])){
-                                $sql = "select * from employe where empl_ID =". $_SESSION['empl_ID']."";
-                                $req = mysqli_query($bdd, $sql);
-                                $rlt = mysqli_fetch_assoc($req);
-                                if (md5($_POST['lastmdp'])==$rlt['empl_password']){
-                                    if ($_POST['newmdp'] == $_POST['newmdp2']){
-                                        $sql2 = "update employe set empl_password ='".md5($_POST['newmdp'])."' where empl_ID=".$_SESSION['empl_ID']."";
-                                        $req2 = mysqli_query($bdd, $sql2);
-                                        
-                                        header ("Location :#");
-                                        
-                                    }
-                                }
-                                    
-                                    
-                                
-                            }
-                        ?>
-                        
 		</section>
 		
 
