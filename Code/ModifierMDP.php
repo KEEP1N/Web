cf04e30688c83a2f8f8ccb3aca779ad513e4ed24
Aleran974 <f.pierrealexandre@live.fr>
2015-06-15 10:06:47 +0200

    modification mot de passe user

--- Code/ModifierMDP.php
+++ Code/ModifierMDP.php
@@ -3,6 +3,7 @@
 	include'script/fonctionphp.php';
 	include'script/db.php';
 	include 'basehtml.php';
+        include 'script/fonction_verification2.php';
         
                 if(empty($_SESSION['empl_ID'])) {
                     header('Location:index.php');
@@ -10,17 +11,17 @@
 ?>
 
 
-	
+    
 		<section class="paragraphe">
 	
 			<h1> Changement de mot de passe</h1>
-                        <form method="post" action="#">
-			<label for="lastmdp">Ancien mot de passe: </label><br/><input type="text" name="lastmdp" /><br/>
-			<label for="newmdp">Nouveau mot de passe: </label><br/><input type="text" name="newmdp" /><br/>
-			<label for="newmdp">Confirmation du nouveau mot de passe:</label><br/><input type="text" name="newmdp2" /><br/>
+                        <form method="post" action="ModifierMDP.php" onsubmit ="return verifForm(this)">
+			<label for="lastmdp">Ancien mot de passe: </label><br/><input type="password" onblur="verifChamp(this)" name="lastmdp" /><br/>
+			<label for="newmdp">Nouveau mot de passe: </label><br/><input type="password" onblur="verifChamp(this)" name="newmdp" /><br/>
+			<label for="newmdp">Confirmation du nouveau mot de passe:</label><br/><input type="password" onblur="verifChamp(this)" name="newmdp2" /><br/>
 			
                         <a href = "AccueilMonCompte.php"><input type="button" id="retour" value=""/></a>
-			<input type="submit" id="submit" name="validation" value="Modifier" onclick="alert('Félicitations! Votre mot de passe a été changé.')"/>
+			<input type="submit" id="submit" name="validation" value="Modifier"/>
                         </form>
                         <?php 
                     
@@ -34,6 +35,7 @@
                                         $req2 = mysqli_query($bdd, $sql2);
                                         
                                         header ("Location :#");
+                                        
                                     }
                                 }
                                     
@@ -41,6 +43,7 @@
                                 
                             }
                         ?>
+                        
 		</section>
 		
 
